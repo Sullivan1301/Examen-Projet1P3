c26e33fb19dfe19f17facd3051ba398520187f43
Sullivan1301 <hei.sullivan.2@gmail.com>
2024-06-07 14:34:01 +0300

    feat: class Et

--- src/main/java/com/hei/CalculusRatiocinator/Et.java
+++ src/main/java/com/hei/CalculusRatiocinator/Et.java
@@ -1,2 +1,12 @@
-package com.hei.CalculusRatiocinator;public class Et {
+package com.hei.CalculusRatiocinator;
+
+public class Et extends ConjonctionDeCoordination {
+    public Et(Affirmation left, Affirmation right) {
+        super(left, right);
+    }
+
+    @Override
+    public Boolean eval(){
+        return left.eval() && right.eval();
+    }
 }

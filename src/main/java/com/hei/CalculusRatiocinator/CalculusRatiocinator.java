f6b812a71cfe3386081144435a81ff30351f8cdb
Sullivan1301 <hei.sullivan.2@gmail.com>
2024-06-07 16:54:44 +0300

    fix: add toLowerCase

--- src/main/java/com/hei/CalculusRatiocinator/CalculusRatiocinator.java
+++ src/main/java/com/hei/CalculusRatiocinator/CalculusRatiocinator.java
@@ -1,27 +1,29 @@
 package com.hei.CalculusRatiocinator;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
+import java.util.*;
 
 public class CalculusRatiocinator {
-    public static final Map<String, Affirmation> affirmationsMap = new HashMap<String, Affirmation>(){
+    public static final Map<String, Affirmation> affirmationsMap = new HashMap<String, Affirmation>() {
         static {
             affirmationsMap.put("verite_1", new Truth("Lou est beau"));
             affirmationsMap.put("mensonge_1", new Lie("Lou est pauvre"));
             affirmationsMap.put("affirmation_1_vraie", new SimpleAffirmation(true, "Lou est genereux"));
             affirmationsMap.put("affirmation_2_fausse", new SimpleAffirmation(false, "Lou est genereux"));
+
+            affirmationsMap.put("vérité_2", new Truth("Sullivan est intelligent"));
+            affirmationsMap.put("mensonge_2", new Lie("Sullivan est paresseux"));
+            affirmationsMap.put("affirmation_2_true", new SimpleAffirmation(true, "Sullivan est travailleur"));
+            affirmationsMap.put("affirmation_2_false", new SimpleAffirmation(false, "Sullivan est travailleur"));
         }
 
-
-
-        public static Boolean eval(String expression){
+        public static Boolean eval(String expression) {
             String[] tokens = expression.split(" ");
             Stack<Affirmation> stack = new Stack<>();
 
-            for (String token : tokens){
-                token.toLowerCase();
-                switch (token){
+            for (String token : tokens) {
+                token = token.toLowerCase();
+
+                switch (token) {
                     case "et":
                         Affirmation rightEt = stack.pop();
                         Affirmation leftEt = stack.pop();
@@ -38,18 +40,20 @@ public class CalculusRatiocinator {
                         stack.push(new Donc(leftDonc, rightDonc));
                         break;
                     default:
-                        if (affirmationsMap.containsKey(token)){
+                        if (affirmationsMap.containsKey(token)) {
                             stack.push(affirmationsMap.get(token));
                         } else {
-                            throw  new IllegalArgumentException("Unknown" + token);
+                            throw new IllegalArgumentException("Unknown token: " + token);
                         }
                         break;
                 }
             }
-            if (stack.size() !=1){
-                throw new IllegalArgumentException("Invalid expression");
+
+            if (stack.size() != 1) {
+                throw new IllegalStateException("Invalid expression");
             }
+
             return stack.pop().eval();
         }
-    };
-}
+        };
+    }

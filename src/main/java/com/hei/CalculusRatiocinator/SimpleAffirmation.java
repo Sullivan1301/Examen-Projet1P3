31c84835d5314c29bf0b0888d033a0d0a7bc6083
Sullivan1301 <hei.sullivan.2@gmail.com>
2024-06-07 14:02:08 +0300

    feat: class for the simples affirmations

--- src/main/java/com/hei/CalculusRatiocinator/SimpleAffirmation.java
+++ src/main/java/com/hei/CalculusRatiocinator/SimpleAffirmation.java
@@ -1,2 +1,14 @@
-package com.hei.CalculusRatiocinator;public class SimpleAffirmation {
+package com.hei.CalculusRatiocinator;
+
+public class SimpleAffirmation implements Affirmation{
+    private final Boolean value;
+
+    public SimpleAffirmation(Boolean value) {
+        this.value = value;
+    }
+
+    @Override
+    public Boolean eval() {
+        return null;
+    }
 }
